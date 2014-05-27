356533b097317886a15e840a5a3551262fd5547b
Nick Gerakines <nick@gerakines.net>
2014-05-27 14:04:27 -0400

    Adding test cookbook

---
+++ cookbooks/preview_test/README.md
@@ -0,0 +1,13 @@
+# preview_test cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: Nick Gerakines (<nick@gerakines.net>)
---
+++ cookbooks/preview_test/metadata.rb
@@ -0,0 +1,14 @@
+name             'preview_test'
+maintainer       'Nick Gerakines'
+maintainer_email 'nick@gerakines.net'
+license          'MIT'
+description      'Installs/Configures preview_test'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'yum'
+depends 'java'
+depends 'cassandra', '~> 2.2.0'
+depends 'preview_build'
+
+supports 'centos'
