ebe748498de39a289335b12e6d88c4f7f1dd03ea
Chris Antenesse <chris@antenesse.net>
2014-05-29 23:42:11 -0700

    basic signal handling in place

--- preview.go
+++ preview.go
@@ -3,9 +3,17 @@ package main
 import (
 	"github.com/docopt/docopt.go"
 	"github.com/ngerakines/preview/cli"
+	"os"
+	"os/signal"
+	"fmt"
 )
 
 func main() {
+        c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, os.Kill)
+	s := <-c
+	fmt.Println("Got signal:", s)
+        
 	usage := `Preview
 
 Usage: preview [--help --version --config=<file>]
